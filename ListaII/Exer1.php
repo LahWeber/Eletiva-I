d7e8d3ec0bdee1e71cfc830c02dfc92eecb77ddd
laura <lauradossantos098@gmail.com>
2026-09-18 20:26:34 -0300

    Inicio da lista 2

---
+++ ListaII/Exer1.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="PT-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exerc. 1</title>
+</head>
+<body>
+    <h1>Exercício - Soma</h1>
+    <form method="post" action="Exer1r.php">
+        <label for="valor1">Informe o primeiro valor</label>
+        <input type="number" id="valor1" name="valor1">
+<br>
+        <label for="valo2">Informe o segundo valor</label>
+        <input type="number" id="valor2" name="valor2">
+<br>
+        <button type="submit">Enviar</button>
+    </form>
+</body>
+</html>
---
+++ ListaII/Exer1r.php
@@ -0,0 +1,14 @@
+<?php
+    $valor1 = $_POST["valor1"];
+    $valor2 = $_POST["valor2"];
+    $soma = $valor1 + $valor2;
+
+    if($valor1 != $valor2)
+    {
+        echo "O valor da soma é: $soma";
+    }
+    else()
+    {
+        $triplo = ($valor1 + $valor2) * 3;
+        echo "O triplo do resultado é: $triplo"
+    }
